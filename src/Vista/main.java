cc47d994e72541cbf1dd781001562949688dbfc7
CarlosDGiron <carlosdavidgironleonardo@gmail.com>
2023-08-14 16:55:14 -0600

    Formulario terminado. Funnciones por implementar.

---
+++ src/Vista/main.java
@@ -0,0 +1,23 @@
+package Vista;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author cana0
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana1 v= new Ventana1();
+        v.show();
+    }
+    
+}
